5743e6ebe974e5fcee691fa5f490abba62716057
Marcus Jones <marcusnjones@gmail.com>
2019-05-09 22:29:25 -0400

    Create entry model

---
+++ ng/src/app/models/entry.model.ts
@@ -0,0 +1,13 @@
+export interface Entry {
+    id: string;
+    dateCreated: Date;
+    dateUpdated: Date;
+    firstName: string;
+    lastName: string;
+    email: string;
+    address: string;
+    city: string;
+    state: string;
+    zip: number;
+    country: string;
+}
