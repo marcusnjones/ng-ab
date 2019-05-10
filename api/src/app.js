1ccb6a3cb8794488c80049852e060ee9dfd2db66
Marcus Jones <marcusnjones@gmail.com>
2019-05-09 22:18:49 -0400

    Update routing

--- api/src/app.js
+++ api/src/app.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const app = express();
 
-const indexRoute = require('./routes/index');
+const entriesRoute = require('./routes/entries');
 const entryRoute = require('./routes/entry');
 const createRoute = require('./routes/create');
 const updateRoute = require('./routes/update');
 const deleteRoute = require('./routes/delete');
 
-app.use('/', indexRoute);
-app.use('/entry', entryRoute);
-app.use('/create', createRoute);
-app.use('/update', updateRoute);
-app.use('/delete', deleteRoute);
+app.use('/api/entries', entriesRoute);
+app.use('/api/entry', entryRoute);
+app.use('/api/create', createRoute);
+app.use('/api/update', updateRoute);
+app.use('/api/delete', deleteRoute);
 
 module.exports = app;
