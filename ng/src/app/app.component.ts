afe168aba58d43b73db27745615252a05c9dff8b
Marcus Jones <marcusnjones@gmail.com>
2019-05-09 22:22:30 -0400

    Add dispatch to get entries

--- ng/src/app/app.component.ts
+++ ng/src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  OnInit
+} from '@angular/core';
+import {
+  Store,
+  select
+} from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { Entry } from './models/entry.model';
+import { AppState } from './state/app.state';
+import * as fromRoot from './reducers/app.reducer';
+import { ActionTypes } from './actions/app.actions';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ng-ab';
+  entries$: Observable<Entry[]>;
+
+  constructor(private store: Store<AppState>) { }
+
+  ngOnInit() {
+    this.store.dispatch({type: ActionTypes.GetEntries});
+
+    // this.store.pipe(select(fromRoot.selectEntries)).subscribe(
+    //   (entries: Entry[]) => {
+    //     this.entries$ = entries;
+    //   }
+    // );
+  }
 }
